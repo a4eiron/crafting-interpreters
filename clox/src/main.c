ab00a047755e6837454208cdf5d4c474649a3cf1
a4eiron <nagavamsi.root@gmail.com>
2026-07-12 11:23:33 +0530

    stack

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,16 +1,41 @@
 #include "../include/chunk.h"
 #include "../include/debug.h"
+#include "../include/vm.h"
 
 int main(int argc, const char *argv[]) {
 
+    initVM();
     Chunk chunk;
 
     initChunk(&chunk);
-    int constant = addConstant(&chunk, 3.14);
+    int constant = addConstant(&chunk, 1);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, OP_RETURN, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    constant = addConstant(&chunk, 9999);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
     disassembleChunk(&chunk, "test chunk");
+    writeChunk(&chunk, OP_RETURN, 123);
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
